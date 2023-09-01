ba07ec447e255ff228f0acf7d1cd0125b9404143
Tien <tiennv29999@gmail.com>
2023-09-02 01:29:28 +1000

    inital commit

---
+++ js/game-logic.js
@@ -0,0 +1,39 @@
+// All code should be written in this file.
+
+// player one global variables
+let playerOneMoveOneType = undefined;
+let playerOneMoveTwoType = undefined;
+let playerOneMoveThreeType = undefined;
+let playerOneMoveOneValue = undefined;
+let playerOneMoveTwoValue = undefined;
+let playerOneMoveThreeValue = undefined;
+
+// player two global variables
+let playerTwoMoveOneType = undefined;
+let playerTwoMoveTwoType = undefined;
+let playerTwoMoveThreeType = undefined;
+let playerTwoMoveOneValue = undefined;
+let playerTwoMoveTwoValue = undefined;
+let playerTwoMoveThreeValue = undefined;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    if (player == "Player One") {
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveThreeType = moveThreeType;
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeValue = moveThreeValue;
+    } else if (player == "Player Two") {
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveThreeType = moveThreeType;
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeValue = moveThreeValue;
+    }
+}
+
+function getRoundWinner(round) {
+
+}
